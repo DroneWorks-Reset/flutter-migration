94e49d6d3b116defff2949ffdde1ba2e6e107b07
Ajaya Dahal <ajayadahal1000@gmail.com>
2025-03-16 21:38:36 -0500

    Selected work and drone builder button

--- lib/main.dart
+++ lib/main.dart
@@ -49,10 +49,10 @@ class _SideNavState extends State<SideNav> {
               Image.asset('assets/img/logo.png'),
               SizedBox(width: 10),
               Text(
-                "DroneWorkz",
+                "DRONEWORKz",
                 style: TextStyle(color: Colors.white),
               ),
-              SizedBox(width: 700),
+              SizedBox(width: 650),
               ElevatedButton(
                 onPressed: () {
                   _onNavItemTapped(_pages.indexOf('Hire Us'));
@@ -94,7 +94,7 @@ class _SideNavState extends State<SideNav> {
           itemCount: _pages.length,
           scrollDirection: Axis.vertical,  // Set scroll direction to vertical
           itemBuilder: (context, index) {
-            return _buildPageContent(index);
+            return _buildPageContent(index, context);
           },
           onPageChanged: (index) {
             setState(() {
@@ -109,12 +109,12 @@ class _SideNavState extends State<SideNav> {
     );
   }
 
-  Widget _buildPageContent(int index) {
+  Widget _buildPageContent(int index, BuildContext context) {
     switch (_pages[index]) {
       case 'Home':
         return _buildHomePage();
       case 'Works':
-        return _buildWorksPage();
+        return _buildWorksPage(context);
       case 'Drone Builder':
         return _buildDroneBuilderPage();
       case 'About':
@@ -147,6 +147,7 @@ class _SideNavState extends State<SideNav> {
               children: [
                 Column(
                   children: [
+                    SizedBox(width: 5,),
                     Text(
                         'Your next\nUAV/UGV\nbuilders',
                         textAlign: TextAlign.center,
@@ -215,15 +216,37 @@ class _SideNavState extends State<SideNav> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Column(
+                  children: [
                 Text(
-                    "Ajaya Dahal",
+                    "Ajaya Dahal\n",
                     style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(width: 700,),
+                  SizedBox(height: 5,),
                   Text(
-                    "Aaron Uram",
-                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                    "Founder and System Software",
+                    style: TextStyle(color: const Color.fromARGB(255, 223, 223, 223), fontSize: 12),
                   ),
+               
+                  ],
+                ),
+
+                SizedBox(width: 700,),
+                
+                Column(
+                  children: [
+                Text(
+                    "Aaron Uram\n",
+                    style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5,),
+                  Text(
+                    "Founder and Frame Designer",
+                    style: TextStyle(color: const Color.fromARGB(255, 223, 223, 223), fontSize: 12),
+                  ),
+               
+                  ],
+                ),
               ],
             ),
           ],
@@ -232,45 +255,184 @@ class _SideNavState extends State<SideNav> {
     );
   }
 
-  Widget _buildWorksPage() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Selected Work',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
-          SizedBox(height: 20),
-          Text(
-            'S200 Model Powered by NXP flight controller',
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Custom solution for autonomous flying partnered with best parts manufacturers like Gryphon Dynamics, PX4.',
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Custom payloads - Camera, LiDAR. Your requirement; our support.',
-            style: TextStyle(color: Colors.white),
-          ),
-        ],
-      ),
-    );
+  int _currentIndexPic = 0;
+
+  final List<Map<String, String>> imagesData = [
+    {
+      'image': 'assets/img/copter1.jpg',
+      'title': 'S200 Model Powered by NXP flight controller',
+      'description': 'Custom solution for autonomous flying partnered with best parts manufacturers like Gryphon Dynamics, PX4.'
+    },
+    {
+      'image': 'assets/img/copter2.jpg',
+      'title': 'Custom UAV Model',
+      'description': 'Advanced systems for high-performance UAVs designed for various applications.'
+    },
+    {
+      'image': 'assets/img/copter3.jpg',
+      'title': 'Payload Customization',
+      'description': 'Custom payloads such as Camera, LiDAR, tailored to your requirements.'
+    },
+  ];
+
+  void _nextImage() {
+    setState(() {
+      if (_currentIndexPic < imagesData.length - 1) {
+        _currentIndexPic++;
+      } else {
+        _currentIndexPic = 0; // Loop back to the first image
+      }
+    });
   }
 
-  Widget _buildDroneBuilderPage() {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          // Implement navigation to Drone Builder functionality
-        },
-        child: Text('Go to Drone Builder'),
-      ),
-    );
+  void _previousImage() {
+    setState(() {
+      if (_currentIndexPic> 0) {
+        _currentIndexPic--;
+      } else {
+        _currentIndexPic= imagesData.length - 1; // Loop back to the last image
+      }
+    });
   }
+
+ 
+ Widget _buildWorksPage(BuildContext context) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Selected Work',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_left, color: Colors.white, size: 80),
+              iconSize: 80,
+              onPressed: _previousImage,
+            ),
+            SizedBox(width: 10),
+            Expanded(
+                child: SizedBox(
+                  height: 300,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      for (int i = 0; i < imagesData.length; i++)
+                        AnimatedPositioned(
+                          duration: Duration(milliseconds: 300),
+                          left: (i == _currentIndexPic)
+                              ? MediaQuery.of(context).size.width * 0.25
+                              : (i == (_currentIndexPic + 1) % imagesData.length)
+                                  ? MediaQuery.of(context).size.width * 0.5  // Adjusted to ensure the image stays within bounds
+                                  : MediaQuery.of(context).size.width * 0.05,                          
+                           
+                            child: AnimatedContainer(
+                              duration: Duration(milliseconds: 300),
+                              height: (i == _currentIndexPic) ? 300 : 200,
+                              width: (i == _currentIndexPic) ? 300 : 200,
+                              child: Image.asset(
+                                imagesData[i]['image']!,
+                                fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            SizedBox(width: 10),
+            IconButton(
+              icon: Icon(Icons.arrow_right, color: Colors.white, size: 80),
+              onPressed: _nextImage,
+              iconSize: 80,
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        Text(
+          imagesData[_currentIndexPic]['title']!,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 10),
+        Text(
+          imagesData[_currentIndexPic]['description']!,
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+  );
+}
+
+
+
+
+Widget _buildDroneBuilderPage() {
+  bool _isHovered = false;
+  return Center(
+    child: ElevatedButton(
+      onPressed: () {
+        // Implement navigation to Drone Builder functionality
+      },
+      onHover: (isHovered) {
+               setState(() {
+              _isHovered = isHovered;
+                              });
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+                                      (Set<WidgetState> states) {
+                                        if (states.contains(WidgetState.hovered)) {
+                                          return Color.fromARGB(255, 0, 0, 255);
+                                        }
+                                        return Color.fromARGB(170, 0, 0, 255); // Use the component's default.
+                                      },
+                                      ),// Transparent background for gradient effect
+        //elevation: WidgetStateProperty.resolveWith<EdgeInsets?>(0),  // Remove default shadow
+        // shape: RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.circular(30),  // Rounded corners
+        // ),
+        padding: WidgetStateProperty.resolveWith<EdgeInsets?>(
+                                      (Set<WidgetState> states) {
+                                        if (states.contains(WidgetState.hovered)) {
+                                          return EdgeInsets.all(30);
+                                        }
+                                        return EdgeInsets.all(20); // Use the component's default.
+                                      },
+                                  ),
+        // side: BorderSide(color: Colors.blue, width: 2),  // Border color and thickness
+      ),
+      child: Ink(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.grey],  // Gradient colors
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 200, maxHeight: 60),  // Max size for button
+          alignment: Alignment.center,
+          child: Text(
+            'Go to Drone Builder',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,  // Text color
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
 
   Widget _buildAboutPage() {
     return Center(
